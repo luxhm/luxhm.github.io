908c5df9dbcfb2eac6a89fb673d7956842c214a3
luxhm <56641564+luxhm@users.noreply.github.com>
2021-11-22 11:40:14 -0500

    Updates to generateJSON

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
-let characters = [];
+let states = [];
 
-let peeps_csv = fs.readFileSync('data/Characters.csv', 'utf8');
+let states_csv = fs.readFileSync('states.csv', 'utf8');
 
-let peeps = peeps_csv.split("\n");
+let states = states.split("\n");
 
 peeps.forEach(function(peep) {
-  let character_info = peep.split(';');
-  let character = {};
-  character['name'] = character_info[1];
-  character['gender'] = character_info[2];
-  character['house'] = character_info[4];
-  character['species'] = character_info[7];
+  let state_info = peep.split(',');
+  let state = {};
+  state['name'] = state_info[1];
+  state['gender'] = character_info[2];
+  state['house'] = character_info[4];
+  state['species'] = character_info[7];
 
   if (character_info[12])
     character['skills'] = character_info[12].split('|');
