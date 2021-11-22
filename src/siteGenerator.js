d59731e9f4f21e7ed1439f944279e0e1a21b0e0c
cstowe-22 <56417887+cstowe-22@users.noreply.github.com>
2021-11-22 12:04:03 -0500

    File Generation

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let macro_data = fs.readFileSync('data/states.json', 'utf8');
+let macro_data = fs.readFileSync('../data/states.json', 'utf8');
 let macro_template = fs.readFileSync('views/index.ejs', 'utf8');
+let micro_data[] = [fs.readFileSync('../data/states.json', 'utf8');
+let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
 
-let index_html = ejs.render(index_template, {
-  filename: __dirname + '/views/index.ejs',
-  data: JSON.parse(character_info)
+let index_html = ejs.render(macro_template, {
+  filename: __dirname + 'views/index.ejs',
+  data: JSON.parse(macro_data)
 });
 
-fs.writeFileSync('build/index.html', index_html, 'utf8');
+fs.writeFileSync('../build/index.html', index_html, 'utf8');
