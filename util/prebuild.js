45cf41ba40e4b5a398a581c2bfa84807e72dc89c
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2021-11-29 12:49:23 -0500

    Working on deleting the old build folder and copying over static folders

--- util/prebuild.js
+++ util/prebuild.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const path = require("path")
+var fsX = require("fs-extra");
+
 const list = fs.readdirSync('../build');
 
 for(i=0;i<=list.length-1;i++){
@@ -10,4 +13,14 @@ for(i=0;i<=list.length-1;i++){
   }
 }
 
-fs.copyDirectory("../src/images");
+//This needs to copy all of the static folders from the src folder into the new build folder
+var source = '../src/images'
+var destination = '../util/build/images'
+
+fsX.copy(source, destination, function (err) {
+    if (err){
+        console.log('An error occurred while copying the folder.')
+        return console.error(err)
+    }
+    console.log('Copy completed!')
+});
