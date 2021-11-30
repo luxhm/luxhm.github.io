3c6f4ffb4984df1e11894375407839b9e98e6157
cstowe-22 <clifford.stowe22@trinityschoolnyc.org>
2021-11-30 11:05:43 -0500

    Changes

--- util/prebuild.js
+++ util/prebuild.js
@@ -1,11 +1,27 @@
 const fs = require('fs');
-const list = fs.readdirSync('../build');
+const path = require("path")
+//var fsX = require("fs-extra");
+
+const list = fs.readdirSync('./build');
 
 for(i=0;i<=list.length-1;i++){
   try{
-    fs.unlinkSync('../build/'+list[i]);
+    fs.unlinkSync('./build'+list[i]);
   }
   catch(err){
     console.error(err);
   }
 }
+/*
+//This needs to copy all of the static folders from the src folder into the new build folder
+var source = './src/images'
+var destination = './util/build/images'
+
+fsX.copy(source, destination, function (err) {
+    if (err){
+        console.log('An error occurred while copying the folder.')
+        return console.error(err)
+    }
+    console.log('Copy completed!')
+});
+*/
