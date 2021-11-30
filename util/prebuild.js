a0810dd775a75158d5fa264276fe241e21ad5c64
cstowe-22 <56417887+cstowe-22@users.noreply.github.com>
2021-11-29 21:16:33 -0500

    Changes

--- util/prebuild.js
+++ util/prebuild.js
@@ -6,7 +6,7 @@ const list = fs.readdirSync('../build');
 
 for(i=0;i<=list.length-1;i++){
   try{
-    fs.unlinkSync('../build/'+list[i]);
+    fs.unlinkSync('./build'+list[i]);
   }
   catch(err){
     console.error(err);
