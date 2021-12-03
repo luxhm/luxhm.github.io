f02d7a33c50e0babaad17ec9139b9ecdaca75da4
luxhm <56641564+luxhm@users.noreply.github.com>
2021-12-03 11:37:31 -0500

    Getting the server to work

--- build/js/server.js
+++ build/js/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('build'));
+app.use(express.static('static'));
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
